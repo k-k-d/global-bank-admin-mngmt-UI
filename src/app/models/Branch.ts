de5dbd8ef32e895876f5b2ff34dceca23dc6e083
unknown <krishnadhakshin2000@gmail.com>
2022-11-09 21:54:28 +0530

    frontend

---
+++ src/app/models/Branch.ts
@@ -0,0 +1,5 @@
+export class Branch {
+    BranchId = "";
+    BranchName = "";
+    BranchAddress = "";
+}
---
+++ src/app/models/Customer.ts
@@ -0,0 +1,7 @@
+export class Customer {
+    Accountno = "";
+    customerno = "";
+    branchid = "";
+    balance = "";
+    openingdate = '';
+}
